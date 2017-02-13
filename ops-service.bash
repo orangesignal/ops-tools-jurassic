f954b1e25a0165c55bc0065c61a1ba7f60848d54
k-sugisawa <k-sugisawa@TP-IIJ0255.ds.iiji.jp>
2017-02-13 23:06:48 +0900

    不要な物を削除

--- ops-service.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-function include() {
-  declare -r self_dir="$(cd "$(dirname "$BASH_SOURCE")" && pwd)"
-  source "${self_dir}/lib/functions"
-}
-include
-
-ops_service "${1}" "${2}" "${3}"
