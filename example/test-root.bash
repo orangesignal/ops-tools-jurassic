f0397b23b6f682dc12040cf9ffb166b8d71c6c8c
orangesignal <sugisawa@kca.biglobe.ne.jp>
2017-02-19 21:23:18 +0900

    update

---
+++ example/test-root.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+
+list=$(cat <<'END'
+w01
+END
+)
+
+pushd "$(dirname "$BASH_SOURCE")" > /dev/null 2>&1
+
+for host in $(echo "$list"); do
+  result=$(../ops cmd -F ./ssh_config "${host}" 'uname -n')
+  ret=$?
+  if [[ $ret  != 0 ]]; then
+    error "ERROR - ${host}"
+  elif [ "${result}" != "${host}" ]; then
+    echo -e "NG - ${host} <-> ${result}"
+  else
+    echo -e "OK - ${host}"
+  fi
+done
+
+popd > /dev/null 2>&1
