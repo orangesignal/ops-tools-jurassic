1e2d3aa67e156ec9b170d82a3bc4a99ed8d97277
orangesignal <sugisawa@kca.biglobe.ne.jp>
2017-02-20 12:58:03 +0900

    update

--- test/test-fetch.bash
+++ test/test-fetch.bash
@@ -1,16 +1,20 @@
-#!/bin/bash -eu
+#!/bin/bash -euf
 
 pushd "$(dirname "$BASH_SOURCE")" > /dev/null 2>&1
-trap 'popd > /dev/null 2>&1' SIGINT EXIT
+dest_dir="$(pwd)/test-fetch-$(date +%Y%m%d%H%M%S)"
+trap 'rm -rf "${dest_dir}"; popd > /dev/null 2>&1' SIGINT EXIT
+mkdir -p "${dest_dir}"
 
 while read line; do
+  echo "line: $line"
   set -- ${line}
-  dest="$(pwd)/test-fetch-$(date +%Y%m%d%H%M%S)"
-  rm -rf "${dest}"
-  ../ops fetch -F ./ssh_config -q -l 1024 "$@" "${dest}"
-  ret=$?
-  find "${dest}"
-  rm -rf "${dest}"
-done <<'END'
-w01 /var/spool/cron
+  ../ops fetch -F ./ssh_config -q -l 1024 "$@"
+done <<END
+w01 /etc/*.conf     ${dest_dir}/conf
+w01 /var/spool/cron ${dest_dir}
+w01 /etc/init.d/    ${dest_dir}/init.d/
+w01 /var/log/syslog ${dest_dir}
+w01 /var/log/syslog ${dest_dir}/renamed-syslog
 END
+
+find "${dest_dir}"
